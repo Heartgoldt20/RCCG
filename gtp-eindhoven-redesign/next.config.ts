8c216d8a62dcda463f1e3fad3af84fc171ad71f3
Daniel Ideho <59232190+Heartgoldt20@users.noreply.github.com>
2026-08-28 14:49:25 +0200

    Enforce secure content across the site

--- gtp-eindhoven-redesign/next.config.ts
+++ gtp-eindhoven-redesign/next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "upgrade-insecure-requests; block-all-mixed-content",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
